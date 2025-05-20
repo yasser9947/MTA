4ed9e0c2f15537aaf167fefd9ccdec5bb95b3380
hunter <ammar1999h@gmail.com>
2025-05-20 23:32:40 +0300

    add user input validation

--- Main.java
+++ Main.java
@@ -4,22 +4,39 @@ public class Main {
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
-
         Subway.SubwayStations(); //load stations
 
-        System.out.print("Enter the Line you're getting on at (N, L, 6): ");
-        String sourceLine = scanner.nextLine().toUpperCase();
+        String sourceLine, sourceStop, destinationLine, destinationStop;
 
-        System.out.print("Enter the Stop you're getting on at: ");
-        String sourceStop = scanner.nextLine().trim();
+        while (true){
+            System.out.print("Enter the Line you're getting on at (N, L, 6): ");
+            sourceLine = scanner.nextLine().toUpperCase();
+            //check if lines' names exist
+            if(!Subway.subwayLines.containsKey(sourceLine)){
+                System.out.println("One or both lines entered does not exist!");
+                continue;
+            }
 
-        System.out.print("Enter the Line you're getting off at (N, L, 6): ");
-        String destinationLine = scanner.nextLine().toUpperCase();
+            System.out.println("Available stops on line " + sourceLine + ":");
+            System.out.println(String.join(", ", Subway.subwayLines.get(sourceLine)));
+            System.out.print("Enter the Stop you're getting on at: ");
+            sourceStop = scanner.nextLine().trim();
 
-        System.out.print("Enter the Stop you're getting off at: ");
-        String destinationStop = scanner.nextLine().trim();
+            System.out.print("Enter the Line you're getting off at (N, L, 6): ");
+            destinationLine = scanner.nextLine().toUpperCase();
+            if(!Subway.subwayLines.containsKey(destinationLine)){
+                System.out.println("One or both lines entered does not exist!");
+                continue;
+            }
+
+            System.out.println("Available stops on line " + sourceLine + ":");
+            System.out.println(String.join(", ", Subway.subwayLines.get(destinationLine)));
+            System.out.print("Enter the Stop you're getting off at: ");
+            destinationStop = scanner.nextLine().trim();
+        break;
+        }
+
         System.out.println();
-
         Subway.triPlanner(sourceLine, sourceStop, destinationLine, destinationStop);
 
         scanner.close();
